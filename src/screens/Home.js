b305e2a36cc73678d4b54c56a46d4f28e8bbf98c
DeekCS <31720001079@std.bau.edu.jo>
2022-02-05 03:23:56 +0200

    add search and type filter

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -15,6 +15,9 @@ function Home() {
   const [fromDate, setFromDate] = useState();
   const [toDate, setToDate] = useState();
   const [duplicateRooms, setDuplicateRooms] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [type, setType] = useState("all");
+
   useEffect(async () => {
     try {
       setLoading(true);
@@ -40,14 +43,21 @@ function Home() {
     for (const room of duplicateRooms) {
       if (room.currentBookings.length > 0) {
         for (const booking of room.currentBookings) {
-          if (!moment(moment(dates[0]).format("YYYY-MM-DD")).isBetween(booking.fromDate, booking.toDate)
-              && !moment(moment(dates[1]).format("YYYY-MM-DD")).isBetween(booking.fromDate, booking.toDate)
+          if (
+            !moment(moment(dates[0]).format("YYYY-MM-DD")).isBetween(
+              booking.fromDate,
+              booking.toDate
+            ) &&
+            !moment(moment(dates[1]).format("YYYY-MM-DD")).isBetween(
+              booking.fromDate,
+              booking.toDate
+            )
           ) {
             if (
-                moment(dates[0]).format("YYYY-MM-DD") !== booking.fromDate &&
-                moment(dates[0]).format("YYYY-MM-DD") !== booking.toDate &&
-                moment(dates[1]).format("YYYY-MM-DD") !== booking.fromDate &&
-                moment(dates[1]).format("YYYY-MM-DD") !== booking.toDate
+              moment(dates[0]).format("YYYY-MM-DD") !== booking.fromDate &&
+              moment(dates[0]).format("YYYY-MM-DD") !== booking.toDate &&
+              moment(dates[1]).format("YYYY-MM-DD") !== booking.fromDate &&
+              moment(dates[1]).format("YYYY-MM-DD") !== booking.toDate
             ) {
               isAvailable = true;
             }
@@ -61,11 +71,52 @@ function Home() {
     }
   };
 
+  const filterBySearchKey = () => {
+    const tempRooms = duplicateRooms.filter((room) => {
+      return room.name.toLowerCase().includes(searchKey.toLowerCase());
+    });
+    setRooms(tempRooms);
+  };
+
+  const filterByType = (e) => {
+    setType(e);
+    if (e !== "all") {
+      const tempRooms = duplicateRooms.filter((room) => room.type.toLowerCase() === e.toLowerCase());
+      setRooms(tempRooms);
+    } else {
+      setRooms(duplicateRooms);
+    }
+  };
+
   return (
     <div className={"container"}>
-      <div className="row mt-5">
+      <div className="row mt-5  box-shadow justify-content-center align-content-center ">
         <div className="col-md-3 ">
           <RangePicker format={"DD-MM-YYYY"} onChange={filterByDate} />
+        </div>
+        <div className="col-md-3">
+          <input
+            className={"form-control"}
+            placeholder={"Search Rooms ..."}
+            value={searchKey}
+            onChange={(e) => {
+              setSearchKey(e.target.value);
+            }}
+            onKeyUp={filterBySearchKey}
+          />
+        </div>
+        <div className="filter col-md-3">
+          <select
+            className={"form-control"}
+            value={type}
+            onChange={(e) => {
+              filterByType(e.target.value);
+            }}
+          >
+            <option value={"all"}>All</option>
+            <option value={"delux"}>Deluxe</option>
+            <option value={"non-delux"}>Non-Deluxe</option>
+          </select>
         </div>
       </div>
       <div className={"row justify-content-center mt-5"}>
@@ -75,20 +126,23 @@ function Home() {
             <Loader />
           </p>
         )}
-        {rooms.length > 1 && (
+
+        <div>
+          {rooms.map((room) => (
+            <Room
+              key={room.id}
+              room={room}
+              fromDate={fromDate}
+              toDate={toDate}
+            />
+          ))}
+        </div>
+
+        {rooms.length === 0 && (
           <div>
-            {rooms.map((room) => (
-              <Room
-                key={room.id}
-                room={room}
-                fromDate={fromDate}
-                toDate={toDate}
-              />
-            ))}
+            <h1>No Rooms Available</h1>
           </div>
         )}
-
-        {error && <p className={"alert alert-danger"}>{error}</p>}
       </div>
     </div>
   );
